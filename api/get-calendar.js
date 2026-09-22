e03306a3212dd5566db64edf7b8a5fa4ac300e49
RudysPrudys <moravek.rudolf@seznam.cz>
2026-09-22 09:17:55 +0200

    Update get-calendar.js

--- api/get-calendar.js
+++ api/get-calendar.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
 
   // OPRAVA: Změněno caldav.icloud.com na calendars.icloud.com, což obchází HTML přihlašovací smyčku Applu
-  const icloudUrl = "https://p41-caldav.icloud.com/published/2/MTIyNTc4MDU4MjQxMjI1N7HBRe4SrbOMeY3BYc83Tk31hjFDdqWDNGVWlWQfYhaP2CsHUq8WVCaQbZTnxrs1zpRHvU8KsBFr0Qf1zubAb4k";
+  const icloudUrl = "https://p41-caldav.icloud.com/published/2/MTIyNTc4MDU4MjQxMjI1N7HBRe4SrbOMeY3BYc83Tk00_qS7cioqmCe26e9wjXEI7QQzsDADgoUP7pulJyg9tlRP3MPsrl4uTdeXFEymRFI";
 
   try {
     const response = await fetch(icloudUrl, {
